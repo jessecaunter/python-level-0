ecb9175d9a57d2440345d37745232681d9190782
Jesse Caunter <js.caunter@gmail.com>
2021-10-24 19:30:00 +1300

    Create username generator

---
+++ username-generator.py
@@ -0,0 +1,9 @@
+first_name = "jesse"
+last_name = "caunter"
+username = first_name[0] + last_name[:5]
+
+print("First name:", first_name)
+print("Last name:", last_name)
+print("Analysing first and last names...")
+print("Generating username...")
+print("Username:", username)
